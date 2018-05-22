9029f32bcb536942564c5ab259141e58018c786f
mharribey <mathieuharribey@hotmail.fr>
2018-05-22 11:32:10 +0200

    come on

--- minimal.rb
+++ minimal.rb
@@ -22,7 +22,7 @@ gem 'font-awesome-sass', '~> 4.7'
 gem 'sass-rails'
 gem 'simple_form'
 gem 'uglifier'
-gem 'webpacker', '~> 3.5.3'
+gem 'webpacker'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
